e6aae0bf0004ab49de1d52fbfe64a89d57f3cbb0
xiongshengzhi <1220718650@qq.com>
2023-05-02 12:06:06 +0800

    feature: 完成基本框架配置

---
+++ miniprogram/pages/MyMessage/MyMessage.ts
@@ -0,0 +1,77 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msgList: [
+      {
+        title: "帖子: 京东算法面经",
+        avatar: "",
+        msgUser: "超级ohzi、鸡汤来咯"
+      },
+      {
+        title: "帖子: 小学期组队",
+        avatar: "",
+        msgUser: "穿山甲、双料特工"
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage(opts): WechatMiniprogram.Page.ICustomShareContent {
+    console.log(opts.target)
+    return {}
+  }
+})
